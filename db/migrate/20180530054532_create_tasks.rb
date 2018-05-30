d60c92e1dcba8cfc75bd61f7a37196a36e51c16a
kubota <kimchinametake@gmail.com>
2018-05-30 15:45:31 +0900

    create task model

---
+++ db/migrate/20180530054532_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.string :note
+      t.datetime :deadline
+      t.boolean :status
+
+      t.timestamps
+    end
+  end
+end
